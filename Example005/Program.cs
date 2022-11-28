66823bc461ea70135f5b816855168c16e1db36d1
alena <alena_sc@outlook.com>
2022-11-28 13:15:14 -0600

    added info Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Put your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+      Console.WriteLine("Uhha, this is Masha!");  
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
